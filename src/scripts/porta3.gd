71b26738c79a56973c7be12c1c00d2e9305e948c
nnMezza <davidemezzaroma67@gmail.com>
2024-11-21 16:38:36 +0100

    aggiunge tutte le porte e le stanze, tutto fixato

---
+++ src/scripts/porta3.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var entered = -1
+
+func _on_body_entered(body):
+	entered += 1
+
+func _on_body_exited(body):
+	entered -= 1
+
+func _process(_delta):
+	if entered == 1:
+		if Input.is_action_just_pressed("ui_up"):
+			get_tree().change_scene_to_file("res://scenes/room3.tscn")
